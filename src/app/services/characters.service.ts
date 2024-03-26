e8852b69acf4697cfda1275a538bdae72f19c9c9
Ignas Davulis <ignas.davulis@swedbank.lt>
2024-03-26 12:22:02 +0200

    rxjs init

--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -1,21 +1,62 @@
-import { Injectable, inject } from '@angular/core';
-import { Character, Characters, CharactersRes } from '../types';
+import { Injectable, OnDestroy, inject } from '@angular/core';
+import { Character, Characters, CharactersRes } from './types';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  Subject,
+  map,
+  takeUntil,
+  tap,
+} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CharactersService {
+export class CharactersService implements OnDestroy {
+  characters$ = new BehaviorSubject<Characters>([]);
+  #favoriteIds: number[] = [];
   private readonly client = inject(HttpClient);
+  private readonly destroy$ = new Subject<void>();
 
-  getCharacters(): Observable<CharactersRes> {
-    return this.client.get<CharactersRes>(
-      'https://rickandmortyapi.com/api/character',
+  getCharacters(): void {
+    this.client
+      .get<CharactersRes>('https://rickandmortyapi.com/api/character')
+      .pipe(
+        map((response: CharactersRes) => response.results),
+        tap((characters) => this.characters$.next(characters)),
+        takeUntil(this.destroy$),
+      )
+      .subscribe();
+  }
+
+  getCharacter(id: string): Observable<Character | undefined> {
+    return this.characters$.pipe(
+      map((characters) =>
+        characters.find((charcter: Character) => charcter.id === +id),
+      ),
     );
   }
 
-  // getCharacter(id: string): Character {
-  //   return this.getCharacters().find((character) => character.id === +id)!;
-  // }
+  filterCharacters(name: string): void {
+    this.client
+      .get<CharactersRes>('https://rickandmortyapi.com/api/character')
+      .pipe(
+        map((response: CharactersRes) => response.results),
+        map((characters: Characters) =>
+          characters.filter((character) =>
+            character.name.toLowerCase().includes(name.toLowerCase()),
+          ),
+        ),
+        tap((characters) => this.characters$.next(characters)),
+        takeUntil(this.destroy$),
+      )
+      .subscribe();
+  }
+
+  toggleFavorite(id: number): void {}
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
 }
